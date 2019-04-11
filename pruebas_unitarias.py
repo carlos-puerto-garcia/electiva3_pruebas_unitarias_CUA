245ac30ef7466f14a903f796d343d10798dc7d4c
AlexisC4r <oquendoomar4600@coruniamericana.edu.co>
2019-04-10 19:57:48 -0500

    Omar Alexis Oquendo Cárdenas

--- pruebas_unitarias.py
+++ pruebas_unitarias.py
@@ -2,6 +2,7 @@ import unittest
 import Test_Carlos_PuertoG
 import Test2_Carlos_PuertoG
 import Edwin
+import Cardenas
 
 class Test_ejemplo(unittest.TestCase):
     def test_suma(self):
@@ -19,9 +20,13 @@ class Test_multiplicacion(unittest.TestCase):
 class Test_cuadrado(unittest.TestCase):
     def test_cuadrado2(self):
         result=Edwin.cuadrado2(5)
-        self.assertEqual(result,25)        
-
+        self.assertEqual(result,25)
         
+class Test(unittest.TestCase):
+    def test_sumaprogre(self):
+        result=Cardenas.sumaprogre(5)
+        self.assertEqual(result,15)
+       
 
 
 if __name__== '__main__':
